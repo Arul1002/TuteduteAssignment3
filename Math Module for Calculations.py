b36b9ea9f5fab4b9fa181fba033a3775700bc580
Arul1002 <arulmani1025@gmail.com>
2025-04-22 06:54:38 +0530

    update

---
+++ Math Module for Calculations.py
@@ -0,0 +1,8 @@
+import math
+number = float(input("Enter a number: "))
+square_root = math.sqrt(number)
+natural_log = math.log(number)
+sine_value = math.sin(number)
+print(f"The square root of {number} is {square_root:.2f}")
+print(f"The logarithm of {number} is {natural_log:.2f}")
+print(f"The sine of {number} is {sine_value:.2f}")
